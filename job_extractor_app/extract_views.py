b640e48598826521368a7fcbba18ed6a9325bf24
yassineraddaoui <yassineraddaouicontact@gmail.com>
2024-09-21 18:00:41 +0100

    refactor auth garbage

--- job_extractor_app/extract_views.py
+++ job_extractor_app/extract_views.py
@@ -4,9 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 import requests
 from bs4 import BeautifulSoup
-from .models import OffresPublic
-from rest_framework.decorators import renderer_classes
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+
 from .offre import JobOffer
 
 logger = logging.getLogger(__name__)
@@ -14,71 +12,6 @@ logger = logging.getLogger(__name__)
 def handle_exception(e):
     logger.error(f"An error occurred: {str(e)}")
     return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-def get_keycloak_users(token):
-    # URL to fetch users from Keycloak (replace with your Keycloak URL and realm)
-    keycloak_url = "http://localhost:8080/admin/realms/master/users"
-
-    headers = {
-        "Authorization": f"Bearer {token}",  # Pass the token in the Authorization header
-        "Content-Type": "application/json"
-    }
-
-    response = requests.get(keycloak_url, headers=headers)
-    print(response)
-
-    if response.status_code == 200:
-        return response.json() 
-    else:
-        raise Exception(f"Error fetching users from Keycloak: {response.status_code}")
-
-def get_user_tasks(user_id):
-    # Query the database for tasks associated with the given Keycloak user ID
-    tasks = ['task1', 'task2']
-    return tasks
-
-@api_view(['POST'])
-def get_users_with_tasks(request):
-    # Extract the Authorization header
-    auth_header = request.headers.get('Authorization')
-
-    # Ensure that the Authorization header is present and starts with "Bearer "
-    if not auth_header or not auth_header.startswith('Bearer '):
-        return Response({"error": "Authorization token not provided or invalid"}, status=401)
-    
-    # Extract the token (remove "Bearer " from the header)
-    token = auth_header.split(' ')[1]
-
-    try:
-        # Use the token to fetch users from Keycloak
-        users = get_keycloak_users(token)
-
-        result = []
-
-        # Loop through the Keycloak users and fetch tasks for each user
-        for user in users:
-            user_id = user['id']  # Keycloak user ID
-            tasks = get_user_tasks(user_id)  # Fetch tasks for this user
-
-            user_info = {
-                "user_id": user_id,
-                "username": user['username'],
-                "email": user.get('email', 'No email provided'),
-                "tasks": []  # Convert tasks queryset to a list of dictionaries
-            }
-            result.append(user_info)
-
-        return Response(result)  # Return the combined result as JSON
-
-    except Exception as e:
-        # Handle any errors that occur while fetching users or tasks
-        return Response({"error": str(e)}, status=500)
-
-
-@api_view(['GET'])
-def find_postulation_by_id(id_postulation):
-    return get_object_or_404(Postulation, id=id_postulation)
-
-
 
 def offer_to_dict(offer):
     return {
@@ -176,7 +109,7 @@ def keejob_jobs(request):
 
 
 @api_view(['GET'])
-def open(request):
+def opencarrier_jobs(request):
     url = "https://www.optioncarriere.tn/recherche/emplois?s=&l=Tunisie"
     html_content = fetch_html_content(url)
 
--- job_extractor_app/urls.py
+++ job_extractor_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('offres/public/linkedin', extract_views.linkedin_jobs, name='linkedin_offres'),
     path('offres/public/keejob', extract_views.keejob_jobs, name='keejob_jobs'),
-    path('offres/public/optioncarrier', extract_views.open, name='optioncarriere_jobs'),
+    path('offres/public/optioncarrier', extract_views.opencarrier_jobs, name='optioncarriere_jobs'),
 
 
 ]
